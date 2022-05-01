7e264424a7bf849604a0688037dbff215db2f9ee
mreyeshn96 <mreyeshn96dev@gmail.com>
2022-04-30 20:47:31 -0600

    routes refactorice

---
+++ src/modules/CharacterCreator/router/index.js
@@ -0,0 +1,4 @@
+export default {
+    name: 'characterCreator',
+    component: () => import('@/modules/CharacterCreator/views/index.vue'),
+}
--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,21 @@
 import { createRouter, createWebHashHistory } from 'vue-router';
 
 /**
+ * Components route
+*/
+import characterCreatorRouter from '@/modules/CharacterCreator/router/index.js';
+
+/**
  * Routes
  */
 const routes = [
     {
         path: '/',
         component: () =>  import( /* webpackChunkName: "App" */ '@/App.vue')
+    },
+    {
+        path: '/character/creator',
+        ...characterCreatorRouter
     }
 ]
 
