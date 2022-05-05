50e2b73230ce79f1b1b8e812c4216a34fb5f2801
mreyeshn96 <mreyeshn96dev@gmail.com>
2022-05-05 10:50:08 -0600

    Hair styles

--- src/modules/CharacterCreator/utils/FemaleHairStyles.js
+++ src/modules/CharacterCreator/utils/FemaleHairStyles.js
@@ -1,319 +1,319 @@
 const femaleHairStyles = [
   {
     id: 0,
-    image_name: "Clothing_F_2_0.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_0.webp",
   },
   {
     id: 1,
-    image_name: "Clothing_F_2_1.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_1.webp",
   },
   {
     id: 10,
-    image_name: "Clothing_F_2_10.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_10.webp",
   },
   {
     id: 11,
-    image_name: "Clothing_F_2_11.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_11.webp",
   },
   {
     id: 12,
-    image_name: "Clothing_F_2_12.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_12.jpg",
   },
   {
     id: 13,
-    image_name: "Clothing_F_2_13.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_13.webp",
   },
   {
     id: 14,
-    image_name: "Clothing_F_2_14.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_14.webp",
   },
   {
     id: 15,
-    image_name: "Clothing_F_2_15.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_15.jpg",
   },
   {
     id: 16,
-    image_name: "Clothing_F_2_16.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_16.webp",
   },
   {
     id: 17,
-    image_name: "Clothing_F_2_17.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_17.jpg",
   },
   {
     id: 18,
-    image_name: "Clothing_F_2_18.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_18.jpg",
   },
   {
     id: 19,
-    image_name: "Clothing_F_2_19.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_19.jpg",
   },
   {
     id: 2,
-    image_name: "Clothing_F_2_2.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_2.webp",
   },
   {
     id: 20,
-    image_name: "Clothing_F_2_20.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_20.webp",
   },
   {
     id: 21,
-    image_name: "Clothing_F_2_21.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_21.webp",
   },
   {
     id: 22,
-    image_name: "Clothing_F_2_22.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_22.jpg",
   },
   {
     id: 23,
-    image_name: "Clothing_F_2_23.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_23.jpg",
   },
   {
     id: 25,
-    image_name: "Clothing_F_2_25.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_25.jpg",
   },
   {
     id: 26,
-    image_name: "Clothing_F_2_26.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_26.jpg",
   },
   {
     id: 27,
-    image_name: "Clothing_F_2_27.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_27.jpg",
   },
   {
     id: 28,
-    image_name: "Clothing_F_2_28.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_28.jpg",
   },
   {
     id: 29,
-    image_name: "Clothing_F_2_29.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_29.jpg",
   },
   {
     id: 3,
-    image_name: "Clothing_F_2_3.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_3.webp",
   },
   {
     id: 30,
-    image_name: "Clothing_F_2_30.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_30.jpg",
   },
   {
     id: 31,
-    image_name: "Clothing_F_2_31.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_31.jpg",
   },
   {
     id: 32,
-    image_name: "Clothing_F_2_32.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_32.jpg",
   },
   {
     id: 33,
-    image_name: "Clothing_F_2_33.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_33.jpg",
   },
   {
     id: 34,
-    image_name: "Clothing_F_2_34.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_34.jpg",
   },
   {
     id: 35,
-    image_name: "Clothing_F_2_35.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_35.jpg",
   },
   {
     id: 36,
-    image_name: "Clothing_F_2_36.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_36.jpg",
   },
   {
     id: 37,
-    image_name: "Clothing_F_2_37.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_37.jpg",
   },
   {
     id: 38,
-    image_name: "Clothing_F_2_38.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_38.jpg",
   },
   {
     id: 39,
-    image_name: "Clothing_F_2_39.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_39.jpg",
   },
   {
     id: 4,
-    image_name: "Clothing_F_2_4.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_4.webp",
   },
   {
     id: 40,
-    image_name: "Clothing_F_2_40.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_40.jpg",
   },
   {
     id: 41,
-    image_name: "Clothing_F_2_41.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_41.jpg",
   },
   {
     id: 42,
-    image_name: "Clothing_F_2_42.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_42.jpg",
   },
   {
     id: 43,
-    image_name: "Clothing_F_2_43.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_43.webp",
   },
   {
     id: 44,
-    image_name: "Clothing_F_2_44.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_44.jpg",
   },
   {
     id: 45,
-    image_name: "Clothing_F_2_45.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_45.jpg",
   },
   {
     id: 46,
-    image_name: "Clothing_F_2_46.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_46.jpg",
   },
   {
     id: 47,
-    image_name: "Clothing_F_2_47.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_47.jpg",
   },
   {
     id: 48,
-    image_name: "Clothing_F_2_48.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_48.jpg",
   },
   {
     id: 49,
-    image_name: "Clothing_F_2_49.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_49.jpg",
   },
   {
     id: 5,
-    image_name: "Clothing_F_2_5.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_5.webp",
   },
   {
     id: 50,
-    image_name: "Clothing_F_2_50.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_50.jpg",
   },
   {
     id: 52,
-    image_name: "Clothing_F_2_52.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_52.jpg",
   },
   {
     id: 53,
-    image_name: "Clothing_F_2_53.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_53.jpg",
   },
   {
     id: 54,
-    image_name: "Clothing_F_2_54.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_54.jpg",
   },
   {
     id: 55,
-    image_name: "Clothing_F_2_55.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_55.jpg",
   },
   {
     id: 56,
-    image_name: "Clothing_F_2_56.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_56.jpg",
   },
   {
     id: 57,
-    image_name: "Clothing_F_2_57.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_57.jpg",
   },
   {
     id: 58,
-    image_name: "Clothing_F_2_58.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_58.jpg",
   },
   {
     id: 59,
-    image_name: "Clothing_F_2_59.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_59.jpg",
   },
   {
     id: 6,
-    image_name: "Clothing_F_2_6.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_6.jpg",
   },
   {
     id: 60,
-    image_name: "Clothing_F_2_60.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_60.jpg",
   },
   {
     id: 61,
-    image_name: "Clothing_F_2_61.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_61.jpg",
   },
   {
     id: 62,
-    image_name: "Clothing_F_2_62.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_62.jpg",
   },
   {
     id: 63,
-    image_name: "Clothing_F_2_63.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_63.jpg",
   },
   {
     id: 64,
-    image_name: "Clothing_F_2_64.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_64.jpg",
   },
   {
     id: 65,
-    image_name: "Clothing_F_2_65.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_65.jpg",
   },
   {
     id: 66,
-    image_name: "Clothing_F_2_66.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_66.jpg",
   },
   {
     id: 67,
-    image_name: "Clothing_F_2_67.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_67.jpg",
   },
   {
     id: 68,
-    image_name: "Clothing_F_2_68.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_68.jpg",
   },
   {
     id: 69,
-    image_name: "Clothing_F_2_69.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_69.jpg",
   },
   {
     id: 7,
-    image_name: "Clothing_F_2_7.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_7.webp",
   },
   {
     id: 70,
-    image_name: "Clothing_F_2_70.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_70.jpg",
   },
   {
     id: 71,
-    image_name: "Clothing_F_2_71.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_71.jpg",
   },
   {
     id: 72,
-    image_name: "Clothing_F_2_72.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_72.jpg",
   },
   {
     id: 73,
-    image_name: "Clothing_F_2_73.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_73.jpg",
   },
   {
     id: 74,
-    image_name: "Clothing_F_2_74.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_74.jpg",
   },
   {
     id: 75,
-    image_name: "Clothing_F_2_75.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_75.jpg",
   },
   {
     id: 76,
-    image_name: "Clothing_F_2_76.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_76.jpg",
   },
   {
     id: 77,
-    image_name: "Clothing_F_2_77.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_77.jpg",
   },
   {
     id: 78,
-    image_name: "Clothing_F_2_78.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_78.jpg",
   },
   {
     id: 79,
-    image_name: "Clothing_F_2_79.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_79.webp",
   },
   {
     id: 8,
-    image_name: "Clothing_F_2_8.jpg",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_8.jpg",
   },
   {
     id: 80,
-    image_name: "Clothing_F_2_80.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_80.webp",
   },
   {
     id: 9,
-    image_name: "Clothing_F_2_9.webp",
+    full_path: "/src/assets/images/game/hairstyles/female/Clothing_F_2_9.webp",
   },
 ];
 
